b9034aad8b8d9b2c8eec916f8c4ec0945da33436
gootibi <gootibi@gmail.com>
2023-12-24 01:18:55 +0100

    Create Firebase database Changes to be committed: 	modified:   src/components/Chat.jsx 	modified:   src/components/Message.jsx 	modified:   src/firebase.js

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 
 const style = {
-    message: `flex items-center shadow-xl m-4 py-2 px-3 rounded-tl-full rounded-tr-full`,
-    name: `absolute mt-[-4rem] text-gray-600 text-xs`,
-    sent: `bg-[#395dff] text-white flex-row-reverse text-end float-right rounded-bl-full`,
-    received: `bg-[#e5e5ea] text-black float-left rounded-br-full`,
+  message: `flex items-center shadow-xl m-4 py-2 px-3 rounded-tl-full rounded-tr-full`,
+  name: `absolute mt-[-4rem] text-gray-600 text-xs`,
+  sent: `bg-[#395dff] text-white flex-row-reverse text-end float-right rounded-bl-full`,
+  received: `bg-[#e5e5ea] text-black float-left rounded-br-full`,
 };
 
 
-const Message = () => {
+const Message = ({ message }) => {
   return (
     <div>
-        <div className={style.message}>
-            <p className={style.name}>Dave</p>
-            <p className="">I'm learning REACT!</p>
-        </div>
+      <div className={style.message}>
+        <p className={style.name}>Dave</p>
+        <p className="">{message.text}</p>
+      </div>
     </div>
   )
 }
